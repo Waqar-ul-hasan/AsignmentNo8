8996f8c20bec8b4664e5fbf002f6634a8e34d3da
Hafiz Haris <harismubeen74@gmail.com>
2023-02-13 00:05:41 +0500

    Asignment No 8

---
+++ script.js
@@ -0,0 +1,77 @@
+let Name= "Umair Azmat";
+let admin=Name 
+console.log(Name)
+
+let planet= "Earth";
+console.log(planet)
+
+let userName=""
+console.log(userName)
+
+let drink=("Coka kola")
+console.log(drink)
+
+let favNum=("25")
+console.log(favNum)
+
+function myFavs(){
+  development
+
+}
+console.log(myFavs);
+
+let firstname= ("Umair Azmat");
+let age=("20");
+
+console.log(firstname,age);
+
+function power(a,b){
+    return a+b
+}
+console.log(power(2,9));
+
+
+function power1(a,b){
+    return a/b
+}
+console.log(power1(5,9));
+
+
+function power2(a,b,c){
+    return a+b/c
+}
+console.log(power2(2,9,5));
+
+
+function power3(a,b,c){
+    return a*-b+c
+}
+console.log(power3(1.8,32,32));
+
+let a=+prompt("Enter Value For Addition")
+let b=+prompt("Enter Value 2 For Addition")
+let c = a + b;
+alert(c)
+
+
+let value1=+prompt("Enter Value For Subtraction")
+let value2=+prompt("Enter Value 2 For Subtraction")
+let ans3 = a + b;
+console.log(ans3)
+
+let ab=+prompt("Enter Your Value For Multiply")
+let bc=+prompt("Enter Your 2 Value For Multiply")
+let cd= ab*bc;
+alert(cd)
+
+
+let num1=+prompt("Enter Your Value For Quotient")
+let num2=+prompt("Enter Your 2 Value For Quotient")
+let ans= num1 / num2;
+console.log(ans)
+
+
+let num3=+prompt("Enter Your Value For Reminder")
+let num4=+prompt("Enter Your 2 Value For Reminder")
+let ans2= num3 % num4;
+alert(ans2)
